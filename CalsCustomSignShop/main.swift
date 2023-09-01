c0d33786bc715bdad48bd60ace407df3183c1d26
Alex-Gois <alex.oli.gois@gmail.com>
2023-09-01 16:07:48 -0300

    Adicionado o exercício CalsCustomSingShop

--- CalsCustomSignShop/main.swift
+++ CalsCustomSignShop/main.swift
@@ -1,11 +1,38 @@
-//
-//  main.swift
-//  CalsCustomSignShop
-//
-//  Created by Alex Gois on 01/09/23.
-//
-
 import Foundation
 
-print("Hello, World!")
 
+// TODO: define the 'birthday' String constant
+let birthday: String = "Birthday"
+
+// TODO: define the 'valentine' String constant
+let valentine: String = "Valentine's Day"
+
+// TODO: define the 'anniversary' String constant
+let anniversary: String = "Anniversary"
+
+// TODO: define the 'space' Character constant
+let space: Character = " "
+
+// TODO: define the 'exclamation' Character constant
+let exclamation: Character = "!"
+
+//Task 3
+func buildSign(for occasion: String, name: String) -> String {
+    let happy: String = "Happy"
+    let phrases: String = "\(happy) \(occasion) \(name)!"
+    return phrases
+}
+
+//Task 4
+func graduationFor(name: String, year: Int) -> String {
+    let congratulation: String = "Congratulations"
+    let graduation: String = "\(congratulation) \(name)!\nClass of \(year)"
+    return graduation
+}
+
+//Task 5
+func costOf(sign: String) -> Int {
+    let basePrice: Int = 20
+    let price: Int = (sign.count * 2) + basePrice
+    return price
+}
